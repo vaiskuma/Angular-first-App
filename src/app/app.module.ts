4c7fbedda3cc145e4cd53c341d5389e4cd9acb4d
vaiskuma <bakijanova@gmail.com>
2018-04-25 11:44:13 +0200

    loggin added(social and google)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
+import { UserResolver } from './user/user.resolver';
+import { AuthGuard } from './core/auth.guard';
+import { AuthService } from './core/auth.service';
+import { UserService } from './core/user.service';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-//import { ArticleComponent } from './article/article.component';
+import { CreateLinkComponent } from './create-link/create-link.component';
+import {ArticleComponent} from './article/article.component';
 import {ArticleData} from './article/article.model';
-import { ArticleComponent} from './article/article.component';
+import { FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticleComponent
+    LoginComponent,
+    UserComponent,
+    RegisterComponent,
+    CreateLinkComponent,
+    ArticleComponent,
+
+
+
   ],
-  //entryComponents: [ArticleComponent],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: false }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
-  providers: [],
+  providers: [AuthService, UserService, UserResolver, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/create-link/create-link.component.ts
@@ -0,0 +1,46 @@
+import { Component, HostBinding } from '@angular/core';
+import { ArticleData} from '../article/article.model';
+import { ArticleComponent} from '../article/article.component';
+
+@Component({
+  selector: 'create-link-root',
+  templateUrl: 'create-link.component.html',
+  styleUrls: ['create-link.component.scss'],
+
+})
+export class CreateLinkComponent {
+  //nothing: Array<ArticleData>;
+  nothing: ArticleData[];
+  titleInput:string = "example";
+
+  addArticle(title: HTMLInputElement, link:HTMLInputElement):boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.nothing.push(new ArticleData(title.value,link.value,0));
+    //clears the input values
+    title.value= '';
+    link.value = " ";
+
+    return false;
+
+  }
+
+
+  constructor() {
+    this.nothing = [
+     new ArticleData('Angular 2', 'http://angular.io/docs',3),
+      new ArticleData ('Fullstack', 'http://fullstack.io',6),
+      new ArticleData ('Angular Homepage', 'http.angula.io/hello',1),
+      new ArticleData ('Stack Overflow', 'https.stackoverflow.com/questions/4444477/how-to-tell-if-a-string-contains-a-certain-character-in-javascript#4444497',10)
+    ]
+  }
+
+  compareNumbers(a, b) {
+    return a - b;
+  }
+
+  sortedArticles(): ArticleData[] {
+    return this.nothing.sort(((a:ArticleData, b:ArticleData)=>b.votes - a.votes));
+  }
+
+
+}
